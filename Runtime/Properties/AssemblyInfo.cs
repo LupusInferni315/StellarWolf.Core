1b9709b620eebe60e750cc8c0e458bb97cdb147d
Raistlin Wolfe <jdoonan61@gmail.com>
2023-02-05 07:46:02 -0700

    Update AssemblyInfo.cs

--- Runtime/Properties/AssemblyInfo.cs
+++ Runtime/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("49276D20-6120-676F-6F64-206461646479")]
 
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: SuppressMessage("Style", "IDE0220:Add explicit cast", Justification = "https://github.com/dotnet/roslyn/issues/63470")]
 [assembly: SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "<Pending>", Scope = "member", Target = "~M:StellarWolf.ChaosEngine.NextByte(System.Byte,System.Byte)~System.Byte")]
