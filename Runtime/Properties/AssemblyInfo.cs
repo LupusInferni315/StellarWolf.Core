c3aeeab2d3e8dfd8262910396e7f367de03754a4
Raistlin Wolfe <jdoonan61@gmail.com>
2023-02-05 06:22:10 -0700

    Create AssemblyInfo.cs

---
+++ Runtime/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StellarWolf.Core")]
+[assembly: AssemblyDescription("Core collection of Stellar Engine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Stellar Wofl")]
+[assembly: AssemblyProduct("StellarWolf.Core")]
+[assembly: AssemblyCopyright("Copyright © Stellar Wolf 2023")]
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49276D20-6120-676F-6F64-206461646479")]
+
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+
+[assembly: SuppressMessage("Style", "IDE0220:Add explicit cast", Justification = "https://github.com/dotnet/roslyn/issues/63470")]
+[assembly: SuppressMessage("Style", "IDE0046:Convert to conditional expression", Justification = "<Pending>", Scope = "member", Target = "~M:StellarWolf.ChaosEngine.NextByte(System.Byte,System.Byte)~System.Byte")]
