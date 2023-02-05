4b290461e191ebc8fcfe5291cb9ee87f6ee51b7c
Raistlin Wolfe <jdoonan61@gmail.com>
2023-02-05 06:28:53 -0700

    Create AssemblyInfo.cs

---
+++ Editor/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StellarWolf.Core.Editor")]
+[assembly: AssemblyDescription("Core Editor of Stellar Engine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Stellar Wolf")]
+[assembly: AssemblyProduct("StellarEditor")]
+[assembly: AssemblyCopyright("Copyright © Stellar Wolf 2023")]
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("223FFF79-10EB-4AAD-9ED4-783EE8D3FF3A")]
+
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
